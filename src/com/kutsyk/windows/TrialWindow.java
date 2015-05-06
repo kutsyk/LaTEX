a52163cb1e80145eaaa8fa320b9f48017c193cdd
Vasyl Kutsyk <vasya.kutsyk@gmail.com>
2015-05-06 18:31:55 +0300

    Trial version windows with restriction added

--- src/com/kutsyk/windows/TrialWindow.java
+++ src/com/kutsyk/windows/TrialWindow.java
@@ -1,5 +1,6 @@
 package com.kutsyk.windows;
 
+import java.awt.event.*;
 import com.kutsyk.security.AES;
 import net.sf.saxon.functions.Parse;
 
@@ -28,19 +29,21 @@ public class TrialWindow extends JFrame {
 
     private File securityDateFile;
     private String dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
-    private String key = "1a25s8fe5dsg65ad";
+//    private String key = "1a25s8fe5dsg65ad";
 //    private String key = "WjgJ8Td76u2Ah42A";
-    private byte[] byte_key;
+//    private byte[] byte_key;
+    private boolean validTrial = false;
+    private int trialLength;
 
     public TrialWindow() {
-        byte_key = key.getBytes();
-        trialVersionCheck();
         initComponents();
+        trialLength = 10;
+        trialVersionCheck();
     }
 
     private void trialVersionCheck() {
         writeDateToFile();
-        checkSecurity();
+        validTrial = isTrialPeriod();
     }
 
     private void writeDateToFile() {
@@ -69,7 +72,7 @@ public class TrialWindow extends JFrame {
         return sdf.format(today);
     }
 
-    private boolean checkSecurity() {
+    private boolean isTrialPeriod() {
         try {
             BufferedReader reader = new BufferedReader(new FileReader(securityDateFile));
             String line;
@@ -78,11 +81,7 @@ public class TrialWindow extends JFrame {
                 content.append(line);
             reader.close();
             String datesLine = content.toString();
-            if (datesLine.isEmpty())
-                return true;
-            else
-                return checkDates(datesLine);
-
+            return checkDates(datesLine);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -100,9 +99,21 @@ public class TrialWindow extends JFrame {
                 ex.printStackTrace();
             }
         }
-        for (Date d : dates)
-            System.out.println(d.toString());
-        return false;
+        Date today = new Date();
+        Date firstInit = dates.get(0);
+        Date endTrialDate = getFinishDate(firstInit);
+        trialProgressBar.setMaximum(trialLength);
+        int diffInDays = (int)( (today.getTime() - firstInit.getTime())
+                / (1000 * 60 * 60 * 24) );
+        trialProgressBar.setValue(diffInDays);
+        return today.after(dates.get(0)) && today.before(endTrialDate);
+    }
+
+    private Date getFinishDate(Date startDate){
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(startDate);
+        cal.add(Calendar.DATE, trialLength);
+        return cal.getTime();
     }
 
     private Date decryptDate(String date) throws ParseException{
@@ -114,40 +125,59 @@ public class TrialWindow extends JFrame {
         return sdf.parse(strDate);
     }
 
+	private void okButtonActionPerformed(ActionEvent e) {
+        if(validTrial) {
+            openMainWindow();
+        }else
+            System.exit(1);
+	}
+
+    private void openMainWindow(){
+        this.setEnabled(false);
+        this.setVisible(false);
+        new MainWindow().setVisible(true);
+    }
+
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
-        trialProgressBar = new JProgressBar();
-        okButton = new JButton();
+		trialProgressBar = new JProgressBar();
+		okButton = new JButton();
 
-        //======== this ========
-        setTitle("Trial LaTEX");
-        setFont(new Font("Times New Roman", Font.PLAIN, 14));
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        Container contentPane = getContentPane();
+		//======== this ========
+		setTitle("Trial LaTEX");
+		setFont(new Font("Times New Roman", Font.PLAIN, 14));
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		Container contentPane = getContentPane();
 
-        //---- okButton ----
-        okButton.setText("Ok");
-        okButton.setFont(new Font("Times New Roman", Font.PLAIN, 14));
+		//---- okButton ----
+		okButton.setText("Ok");
+		okButton.setFont(new Font("Times New Roman", Font.PLAIN, 14));
+        okButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                okButtonActionPerformed(e);
+            }
+        });
 
         GroupLayout contentPaneLayout = new GroupLayout(contentPane);
-        contentPane.setLayout(contentPaneLayout);
-        contentPaneLayout.setHorizontalGroup(
-                contentPaneLayout.createParallelGroup()
-                        .addGroup(GroupLayout.Alignment.TRAILING, contentPaneLayout.createSequentialGroup()
-                                .addContainerGap()
-                                .addGroup(contentPaneLayout.createParallelGroup(GroupLayout.Alignment.TRAILING)
-                                        .addComponent(okButton, GroupLayout.PREFERRED_SIZE, 106, GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(trialProgressBar, GroupLayout.DEFAULT_SIZE, 372, Short.MAX_VALUE))
-                                .addContainerGap())
+		contentPane.setLayout(contentPaneLayout);
+		contentPaneLayout.setHorizontalGroup(
+			contentPaneLayout.createParallelGroup()
+				.addGroup(GroupLayout.Alignment.TRAILING, contentPaneLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(contentPaneLayout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+                                .addComponent(okButton, GroupLayout.PREFERRED_SIZE, 106, GroupLayout.PREFERRED_SIZE)
+                                .addComponent(trialProgressBar, GroupLayout.DEFAULT_SIZE, 372, Short.MAX_VALUE))
+                        .addContainerGap())
         );
         contentPaneLayout.setVerticalGroup(
-                contentPaneLayout.createParallelGroup()
-                        .addGroup(contentPaneLayout.createSequentialGroup()
-                                .addContainerGap()
-                                .addComponent(trialProgressBar, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(okButton, GroupLayout.DEFAULT_SIZE, 30, Short.MAX_VALUE)
-                                .addContainerGap())
+			contentPaneLayout.createParallelGroup()
+				.addGroup(contentPaneLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(trialProgressBar, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(okButton, GroupLayout.DEFAULT_SIZE, 30, Short.MAX_VALUE)
+                        .addContainerGap())
         );
         pack();
         setLocationRelativeTo(getOwner());
@@ -155,7 +185,7 @@ public class TrialWindow extends JFrame {
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
-    private JProgressBar trialProgressBar;
-    private JButton okButton;
+	private JProgressBar trialProgressBar;
+	private JButton okButton;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
